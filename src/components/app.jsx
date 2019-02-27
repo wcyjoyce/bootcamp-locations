7800e6ddf04d0a917d9f9598092eac20582de645
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-02-28 06:29:47 +0800

    navbar HTML and CSS added

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
 
 import List from "../containers/list.jsx";
+import ActiveCity from "../containers/active_city.jsx";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+    <img src={`https://raw.githubusercontent.com/lewagon/www-images/master/logo-square.png`} />
+    <div className="text">
+      <h1>Le Wagon</h1>
+      <p>Change your life, learn to <span>code</span>.</p>
+    </div>
+    </div>
+  );
+}
 
 const App = () => {
   return (
-    <div className="app">
-      <List />
+    <div>
+      <NavBar />
+      <div className="app">
+        <List />
+        <ActiveCity />
+      </div>
     </div>
   );
 };

f6ad774ed9cacac1fffeecf05a3e334413bb7567
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-02-28 06:29:14 +0800

    activeCity reducer added

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,19 @@
-// External modules
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, combineReducers } from "redux";
 
-// Internal modules
 import App from "./components/app";
 import "../assets/stylesheets/application.scss";
 
 import citiesReducer from "./reducers/cities_reducer.js";
+import activeCityReducer from "./reducers/active_city_reducer.js";
 
-// State and reducers
 const reducers = combineReducers({
-  cities: citiesReducer
+  cities: citiesReducer,
+  activeCity: activeCityReducer,
 });
 
-// Render an instance of the component in the DOM
 ReactDOM.render(
   <Provider store={createStore(reducers)}>
     <App />

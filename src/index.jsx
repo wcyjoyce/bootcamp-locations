93eb021a6d399a57fb9c648f1c3ac8b1778715dd
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-03-02 11:46:28 +0800

    redux logger added

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { logger } from "redux-logger";
 
 import App from "./components/app";
 import "../assets/stylesheets/application.scss";
@@ -9,13 +10,15 @@ import "../assets/stylesheets/application.scss";
 import citiesReducer from "./reducers/cities_reducer.js";
 import activeCityReducer from "./reducers/active_city_reducer.js";
 
+const middlewares = applyMiddleware(logger);
+
 const reducers = combineReducers({
   cities: citiesReducer,
   activeCity: activeCityReducer,
 });
 
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, {}, middlewares)}>
     <App />
   </Provider>,
   document.getElementById("root")
